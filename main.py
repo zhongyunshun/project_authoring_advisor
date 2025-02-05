c870bf76c2d83dbf5532ed47453b50a34d53a04e
AlezHibali <ali.daixin.tian@gmail.com>
2025-02-04 22:14:14 -0500

    feat: add system message as arg in rag_pipeline

--- main.py
+++ main.py
@@ -9,18 +9,16 @@ from config.keys import OPENAI_API_KEY
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 
-def answer_question(question, vector_db):
+def answer_question(question, vector_db, sys_sms):
     """
     Answer a single question using RAG Pipeline.
     """
-    prefix = "Here is a fill in blank question, please generate the answer, only output the answer without giving the original sentence: "
-
-    answer = rag_pipeline(prefix + question, vector_db)
+    answer = rag_pipeline(question, vector_db, sys_sms)
     print(f"Question: {question}")
     print(f"Answer: {answer}")
 
 
-def process_questions(csv_file, vector_db, output_csv_file):
+def process_questions(csv_file, vector_db, sys_sms, output_csv_file):
     """
     Reads questions from csv, process them through RAG pipeline,
     saves the answers in a new CSV file.
@@ -30,9 +28,7 @@ def process_questions(csv_file, vector_db, output_csv_file):
     if "question" not in df.columns:
         raise ValueError("CSV file must contain a 'question' column.")
 
-    prefix = "Here is a fill in blank question, please generate the answer, only output the answer without giving the original sentence: "
-    df["generated_answer"] = df["question"].apply(lambda q: rag_pipeline(prefix + q, vector_db))
-
+    df["generated_answer"] = df["question"].apply(lambda q: rag_pipeline(q, vector_db, sys_sms))
     df.to_csv(output_csv_file, index=False)
     print(f"Processed questions saved to {output_csv_file}.")
 
@@ -47,19 +43,20 @@ def main():
         print("Embeddings loaded from file.")
     else:
         # Read txt, split into chunks, generate and store embeddings, save to file
-        input_file = "./output/output.txt"
+        input_file = "./output/cleaned_17.txt"
         chunks = read_and_split_text(input_file)
 
         vector_db = generate_and_save_embeddings(chunks, embedding_file)
 
     # Step 4: Test the RAG pipeline
+    sys_sms = "Here is a fill in blank question, please generate the answer, only output the answer without giving the original sentence: "
     # test_question = "In 2017 Bercy Wycliffe Workplan, Based on the Infrastructure Hazard Monitoring Program, which site is the highest priority for remedial action in the Region?"  # Example question
     # answer_question(test_question, vector_db)
     
     # Output csv from a qa.csv
-    input_csv_file = "data/QA_pair/fill_in_the_blanks_questions.csv"
-    output_csv_file = "data/QA_pair/generated_qa.csv"
-    process_questions(input_csv_file, vector_db, output_csv_file)
+    input_csv_file = "QA_pair/qa_pair_170_0204/TRCA_All_Files_Combined.csv"
+    output_csv_file = "QA_pair/qa_pair_170_0204/TRCA_All_Files_Combined_output.csv"
+    process_questions(input_csv_file, vector_db, sys_sms, output_csv_file)
 
 
 if __name__ == "__main__":
--- pipelines/rag_pipeline.py
+++ pipelines/rag_pipeline.py
@@ -1,16 +1,20 @@
-from langchain_community.vectorstores import FAISS
 from langchain_community.llms import OpenAI
 from langchain.chains import RetrievalQA
 from langchain.chains.question_answering import load_qa_chain
 from langchain_openai import ChatOpenAI
 
-def rag_pipeline(question, vector_db, top_k=5):
+def rag_pipeline(question, vector_db, system_message=None, top_k=5):
     retriever = vector_db.as_retriever(search_type="similarity", search_kwargs={"k": top_k})
-    # llm = OpenAI(model_name="gpt-4o-mini") #temperature=0.1
     llm = ChatOpenAI(model="gpt-4o-mini")
 
     # Load a question-answering chain
     qa_chain = load_qa_chain(llm, chain_type="stuff")
     retrieval_qa_chain = RetrievalQA(retriever=retriever, combine_documents_chain=qa_chain)
-
-    return retrieval_qa_chain.run(question)
+    
+    # Construct messages format with system prompt
+    messages = [
+        {"role": "system", "content": system_message},
+        {"role": "user", "content": question}
+    ]
+    
+    return retrieval_qa_chain.run(messages)
